44034ffb0b17774f943fcc5a533ecc40358b7bb5
Desarrollo Web <Desarrollo Web@DESKTOP-CO6LR15>
2019-08-01 13:42:21 -0300

    EjercicioBiciccleteria

---
+++ src/ejerciciobicicleteria/Bicicleta.java
@@ -0,0 +1,28 @@
+package ejerciciobicicleteria;
+
+public class Bicicleta {
+    private String nroDeSerie;
+    private String modelo;
+    private int año;
+    private float precio;
+    
+    public Bicicleta(String nroDeSerie, String modelo, int año, float precio) {
+        this.nroDeSerie = nroDeSerie;
+        this.modelo = modelo;
+        this.año = año;
+        this.precio = precio;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getNroDeSerie() {
+        return nroDeSerie;
+    }
+    
+}
---
+++ src/ejerciciobicicleteria/Bicicleteria.java
@@ -0,0 +1,27 @@
+package ejerciciobicicleteria;
+
+import java.util.ArrayList;
+
+public class Bicicleteria {
+    private ArrayList<Bicicleta> bicicletas = new ArrayList<>();
+    private float ganancias;
+    private Integer cantidadDeVentas;
+    
+    public Bicicleteria() {
+        
+    }
+    
+    public void addBicicleta(Bicicleta nuevaBici) {
+        
+    }
+    
+    public void venderBicicleta(Bicicleta bicicleta) {
+        
+    }
+    
+    public String buscarBicicleta(String nroDeSerie) {
+        for (int i = 0; i < bicicletas.size(); i++) {
+            
+        }
+    }
+}
